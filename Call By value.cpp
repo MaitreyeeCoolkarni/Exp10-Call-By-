5f946b238e195dbf8adf9ad0b8cc4a0e30d6e7b9
MaitreyeeCoolkarni <dunkul@gmail.com>
2025-08-12 14:46:24 +0530

    Create Call By value.cpp

---
+++ Call By value.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+ void swap(int x,int y){
+       int temp=x;
+       x=y;
+       y=temp;
+    }
+
+int main() {
+     
+    int a =5;
+    int b=10;
+    
+    swap(a,b);
+    
+    cout<<a<<endl<<b;
+    
+}
+
+//Output
+//5
+//10
